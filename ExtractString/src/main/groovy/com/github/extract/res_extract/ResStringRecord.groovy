918a657edc76332f1a697e0bb0e06a53fb9ee495
zhaoyubetter <zhaoyubetter@126.com>
2018-09-25 18:45:07 +0800

    prepared version v1.1.1

--- ExtractString/src/main/groovy/com/github/extract/res_extract/ResStringRecord.groovy
+++ ExtractString/src/main/groovy/com/github/extract/res_extract/ResStringRecord.groovy
@@ -33,11 +33,11 @@ class ResStringRecord implements ExtractStringResAPI {
     /**
      * 每个语言，都有对应的 strings， 与 string-Array
      * key: String， values(-[en|zh-rTW])?
-     * value: List， [strings, stringArray]
+     * value: List， [strings, stringArray, noTranslateStringItems]
      */
     def langStrings = [:]     // 每个语言对应的 [string,stringArray]
     /**
-     * excel文件名
+     * excel文件全名
      */
     def excelFileName = ""
     /**
@@ -60,10 +60,8 @@ class ResStringRecord implements ExtractStringResAPI {
 //        aa.testGetXml()
 
         def postfix = ['a', 'b', 'c']
-        postfix.each {
-            if (it == 'a') {
-                postfix.remove(it)
-            }
+        postfix.forEach {
+            println(it + ">>>>==")
         }
         println(postfix)
     }
@@ -84,7 +82,8 @@ class ResStringRecord implements ExtractStringResAPI {
     }
 
     private void doWork() {
-        def invalidDir = []     // 无效的目录
+        def invalidDir = []     // 无效的目录，可能配置了一些 values-5678，或者是一些其他不存在的目录
+
         // 遍历各个语言目录，获取资源信息
         postfix.each { it ->
             def filePath = "${resFolderPath}/${it}"
@@ -122,6 +121,7 @@ class ResStringRecord implements ExtractStringResAPI {
             def title = lang.key
             def stringsItems = lang.value[0]
             def stringsArrayItems = lang.value[1]
+            def noTranslateStringItems = lang.value[2]
 
             // --- 2. 创建工作表
             def sheet = createSheet(writeBook, "${title}")
@@ -155,6 +155,15 @@ class ResStringRecord implements ExtractStringResAPI {
                 sheet.mergeCells(0, currentRow, 0, array.value.size + currentRow - 1) // 合并单元格
                 sheet.addCell(cell_key)
             }
+
+            // --- 添加内容 noTranslate, 不需要翻译的
+            currentRow = sheet.rows
+            noTranslateStringItems.eachWithIndex { key, value, index ->
+                def cell_key = new Label(0, index + 1, key)
+                def cell_value = new Label(1, index + 1, value)
+                sheet.addCell(cell_key)
+                sheet.addCell(cell_value)
+            }
         }
 
         // 3.生成对比的excel
@@ -173,16 +182,18 @@ class ResStringRecord implements ExtractStringResAPI {
     private def createDiffSheet(writeBook, langStrings) {
         def defaultStringsItems = [:]
         def defaultStringArrayItems = [:]
-        (defaultStringsItems, defaultStringArrayItems) = langStrings.get("values")
-        postfix.remove("values")      // 移除
+        def defaultStringsItemNoTranslate = [:]
+        (defaultStringsItems, defaultStringArrayItems, defaultStringsItemNoTranslate) = langStrings.get("values")
+        postfix.remove("values")      // 移除默认
 
         // 默认语言，直接return
         if (postfix.size() <= 0) {
             return
         }
 
-        def stringsItems = [:]           // string
-        def stringsArrayItems = [:]     // string-array
+        def stringsItems = [:]            // string
+        def stringsArrayItems = [:]       // string-array
+        def stringsItemNoTranslate = [:]
 
         // --- 创建工作表
         def sheet = createSheet(writeBook, "values_compare")
@@ -196,7 +207,6 @@ class ResStringRecord implements ExtractStringResAPI {
 
         // --- 设置表头（其他语言）
         postfix.eachWithIndex { it, index ->
-            (stringsItems, stringsArrayItems) = langStrings.get(it)
             // --- 设置表头（其他语言）
             def cell_title_value = new Label(index + 2, 0, "${it.substring(it.indexOf('-'))}_value", titleCellFormat)
             sheet.addCell(cell_title_value)
@@ -211,7 +221,7 @@ class ResStringRecord implements ExtractStringResAPI {
 
             // 其他语言
             postfix.eachWithIndex { it, i ->
-                (stringsItems, stringsArrayItems) = langStrings.get(it)
+                (stringsItems, stringsArrayItems, stringsItemNoTranslate) = langStrings.get(it)
                 stringsItems.find { it.key == key }?.each {
                     def cell_other_value = new Label(2 + i, index + 1, it.value)   // 3列1行开始
                     sheet.addCell(cell_other_value)
@@ -219,7 +229,7 @@ class ResStringRecord implements ExtractStringResAPI {
             }
         }
 
-        // --- 添加内容 stringsArrayItems
+        // --- 添加内容 stringsArrayItems（默认语言）
         def currentRow = sheet.rows
         defaultStringArrayItems.eachWithIndex { array, index ->
             currentRow = sheet.rows
@@ -234,7 +244,7 @@ class ResStringRecord implements ExtractStringResAPI {
 
             // 其他语言处理
             postfix.eachWithIndex { it, i ->
-                (stringsItems, stringsArrayItems) = langStrings.get(it)
+                (stringsItems, stringsArrayItems, stringsItemNoTranslate) = langStrings.get(it)
                 def arraysItems = stringsArrayItems.find { it.key == array.key }
                 if (arraysItems != null && arraysItems.value.size == array.value.size) {
                     // 为null或数量不对
@@ -248,6 +258,24 @@ class ResStringRecord implements ExtractStringResAPI {
                     } else {
                         println("string-array [${array.key}] in ${it} size not equals")
                     }
+                }
+            }
+        }
+
+        // --- 添加内容 stringsItem - 不需要翻译的 (默认语言)
+        currentRow = sheet.rows
+        stringsItemNoTranslate.eachWithIndex {key, value, index ->
+            def cell_key = new Label(0, index + 1, key)
+            def cell_value = new Label(1, index + 1, value)
+            sheet.addCell(cell_key)
+            sheet.addCell(cell_value)
+
+            // 其他语言
+            postfix.eachWithIndex { it, i ->
+                (stringsItems, stringsArrayItems) = langStrings.get(it)
+                stringsItems.find { it.key == key }?.each {
+                    def cell_other_value = new Label(2 + i, index + 1, it.value)   // 3列1行开始
+                    sheet.addCell(cell_other_value)
                 }
             }
         }
@@ -280,17 +308,24 @@ class ResStringRecord implements ExtractStringResAPI {
     }
 
     /**
-     * 获取执行xml文件中的所有 string，stringArray，并返回长度为2的list集合
+     * 获取执行xml文件中的所有 string，stringArray，并返回长度为3的list集合
      * @param files xml 文件
      * @return list
      */
     private def handleXmlFiles(List files) {
         def stringItems = [:]           // string           (key,value)
         def arrayArrayItems = [:]       // string-array     (key, [])
+        def noTranslateItems = [:]      //
         files.each { it ->
-            def items = getStrings(it)
+            def items = getStrings(it)[0]
             if (items.size() > 0) {
-                stringItems << items
+                stringItems << items // normal
+            }
+
+            // 不需要翻译的
+            def noTranslate = getStrings(it)[1]
+            if (noTranslate.size() > 0) {
+                noTranslateItems << noTranslate
             }
         }
 
@@ -302,35 +337,45 @@ class ResStringRecord implements ExtractStringResAPI {
             }
         }
 
-        [stringItems, arrayArrayItems]
+        // 普通strings，数组，不需要翻译的
+        [stringItems, arrayArrayItems, noTranslateItems]
     }
 
     /**
      * get all string in specified file
-     * 获取指定文件下的所有string，并返回map，map key 为 string 名称，value 为string的值
+     * 获取指定文件下的所有string，并返回list，list 长度为2，类型为map；
+     * map key 为 string 名称，value 为string的值
      * @param file
      * @return map
      */
-    private Map getStrings(File file) {
+    private def getStrings(File file) {
         println("--------> Get string from file [${file.getParentFile().getName()}/${file.getName()}]")
         def root = new XmlParser().parse(file)
+        def result = []
         def stringItems = [:]
+        def notTranslateItems = [:]   //
         // strings
         root.string?.each { it ->
             def translatableValue = it.attributes()["translatable"]
-            // 检测不需要国际化数组配置
+            def item = it.attributes()["name"]
+            // 过滤掉不需要国际化数组配置
             if (null == translatableValue || Boolean.valueOf(translatableValue)) {
-                def item = it.attributes()["name"]
                 stringItems << [(item): it.text()]
+            } else {
+                notTranslateItems << [(item): it.text()]
             }
         }
         println("string size:${stringItems.size()}")
-        stringItems
+
+        result << stringItems
+        result << notTranslateItems
+
+        result
     }
 
     /**
      * get all string-array in specified file
-     * 获取指定文件下的所有string-array，并返回map，map key 为 string 名称，value 为string[]，或者@string/xxx
+     * 获取指定文件下的所有string-array，并返回map，map key 为 string 名称，value 为string[]，或者@
      * @param file
      * @return map
      */
